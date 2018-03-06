735a0d1534e89b9915134ed8aaef280d6906e693
ye546 <anton_bosse@hotmail.com>
2018-03-06 19:49:16 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,8 +6,6 @@
 #include <cstdlib>
 #include <cstdio>
 #include <ctime>
-#define emp_size 5
-bool checkIfEmployeeAlreadyExists(employee *p);
 
 struct employee {
 	int id;
@@ -16,99 +14,115 @@ struct employee {
 };
 
 struct dataBase {
-	static std::vector<employee> v;
-};
+	std::vector<employee> v;
+}db;
 
 
-
-employee createNewEmployee() {
+void createNewEmployee() {
 	srand(time(NULL));
 
-	employee *emp = new employee;
-	dataBase *db = nullptr;
-
+	struct employee *emp = new employee;
 	std::string name, address, role;
-	int id = (1 + rand() % 99999);
 
-	
+	//enter the employees information
+	printf("--New employee---\n\n");
+	printf("Enter the new employees name: >");
+	std::getline(std::cin, emp->name);
+	printf("\nEnter the new employees role: >");
+	std::getline(std::cin, emp->role);
+	printf("\nEnter the new employees address: >");
+	std::getline(std::cin, emp->address);
+	printf("\nEnter the employees wage: >");
+	std::cin >> emp->wage;
+	printf("\nAssigning ID to the new employee...\n\n");
+	emp->id = (1 + rand() % 99999);
 
-	for (int i = 0; i < 1; i++) {
-		printf("Enter the new employees name: >");
-		std::getline(std::cin, emp->name);
-		printf("\nEnter the new employees role: >");
-		std::getline(std::cin, emp->role);
-		printf("\nEnter the new employees address: >");
-		std::getline(std::cin, emp->address);
-		printf("\nEnter the employees wage: >\n");
-		std::cin >> emp->wage;
-		printf("\nAssigning ID to the new employee...\n");
-		emp->id = id;
-	}
 
-	if (db->v.size() > 0) {
-		for (int i = 0; i < db->v.size(); i++) {
-			if (db->v[i].id == emp->id) {
-				printf("This user already exist, assigning new id...\n");
-				redo:
-				emp->id = id;
-				if (db->v[i].id == emp->id) goto redo;
-			}
+	for (int i = 0; i < db.v.size(); i++) {
+		if (db.v.size() <= 0) { break; }
+		//if we by any unlucky chanse we would get a duplication in Id's
+		if (emp->id == db.v[i].id) {
+			printf("This ID already exist, assigning new id...\n");
+			redo:
+			emp->id = (1 + rand() % 99999);
+			if (emp->id == db.v[i].id) goto redo;
 		}
 	}
 
+	//save the new employee in the database
+	db.v.push_back(*emp);
 
-
-}
-
-bool checkIfEmployeeAlreadyExists() {
-	
-	p = &createNewEmployee();
-	bool good = true;
-	dataBase *db = nullptr;
-
-
-	for (int i = 0; i < db->v.size(); i++) {
-		if (p == &db->v[i]) {
-			printf("does this user already exist?\n");
-			good = false;
-		}
-		if (!good) {
-			return false;
-		}	
-	}
-
-	db->v.push_back(*p);
-	db = nullptr;
-	return true;
+	delete emp;
+	emp = nullptr;
 }
 
 void deleteEmployee() {
-	dataBase *db = nullptr;
-	std::string name;
-	printf("Search for the employee name: >");
-	std::getline(std::cin, name);
-
-	//for (int i = 0; i < db->v.size(); i++) {
-	//	if(name == db->v[i])
-	//}
-}
-
-void editEmployee() {
-
-}
-
-void searchForEmployee() {
-	std::string name;
-	std::getline(std::cin, name);
-	dataBase *db = nullptr;
-	
-	for (int i = 0; i < db->v.size(); i++) {
-		if (name == db->v[i].name);
+	int i;
+	printf("\n\nEnter the employees ID that you'd like to delete: >");
+	std::cin >> i;
+	for (int i = 0; i < db.v.size(); i++) {
+		if (i == db.v[i].id) {
+			db.v.erase(db.v.begin() + i);
+		}
 	}
+}
 
+void getIdOfEmployee() {
+	std::string name;
+	printf("\nEnter the name you'd like to find the ID for: >");
+	std::getline(std::cin, name);
+
+	for (int i = 0; i < db.v.size(); i++) {
+		if (name == db.v[i].name) {
+			printf("name: %s\nid: %i\n", db.v[i].name.c_str(), db.v[i].id);
+		}
+	}
+}
+
+void printEmployees() {
+	if (db.v.size() <= 0) {
+		printf("DB is NULL\n\n\n");
+	}
+	else {
+		for (int i = 0; i < db.v.size(); i++) {
+			printf("\nEmploye %i\n{\n\tName: \t%s\n\tAddress: %s\n\tRole: \t%s\n\tWage: \t%f\n\tId: \t%i\n}\n\n", i, db.v[i].name.c_str(), db.v[i].address.c_str(), db.v[i].role.c_str(), db.v[i].wage, db.v[i].id);
+		}
+	}
 }
 
 int main() {
-	
-	getchar();
+	printf("---Terminal---\n\n");
+	int i;
+
+	while (true) {
+		printf("1. Add a new employee to the database.\n2. Get ID of an employee\n3. List all the employees\n4. Delete an employee\n\n>");
+		std::cin >> i;
+		getchar();// flush stream from \n
+		switch (i) {
+		case 1:
+			srand(time(NULL));
+			createNewEmployee();
+			getchar();// flush stream from \n
+			break;
+		case 2:
+			getIdOfEmployee();
+			break;
+		case 3:
+			printEmployees();
+			break;
+		case 4:
+			deleteEmployee();
+			break;
+		case 5:
+			return EXIT_SUCCESS;
+			break;
+		default: 
+			printf("\nThat is not an option!\n\n"); 
+			break;
+		}
+	}
+
+	system("PAUSE");
+
+	return 0;
 }
